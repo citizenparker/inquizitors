8844067a88b4e0c7fb22d21aa958b902851aed0a
Scott Parker <scott.p.parker@gmail.com>
2012-02-09 07:37:57 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject inquizitors "0.1.0-SNAPSHOT"
+            :description "This is a thing"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [aleph "0.2.0"]
+                           [enlive "1.0.0-SNAPSHOT"]
+                           [clojeopardy "0.5.0"]
+                           [ring/ring-core "0.2.5"]
+                           [ring/ring-jetty-adapter "0.2.5"]
+                           ])
