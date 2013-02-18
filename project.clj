6d8507cf860592fd23ff4abd81d0b2892ef73e83
sparker <scott.p.parker@gmail.com>
2013-02-18 13:50:30 -0600

    Upped the aleph dependency to avoid broken sub-dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "This is a thing"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojurescript "0.0-971"]
-                 [aleph "0.2.1-alpha2-SNAPSHOT"]
+                 [aleph "0.3.0-beta11"]
                  [domina "1.0.0-alpha1"]
                  [ring "1.0.2"]
                  [jayq "0.1.0-SNAPSHOT"]]
